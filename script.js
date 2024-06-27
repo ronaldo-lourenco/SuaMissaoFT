2a3a56046873cabe6a0cf46785da18f3d951fcd0
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-06-27 21:59:03 +0000

    Inclui index, script e style

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaPrincipal = document.querySelector(".caixa-principal") ;
+const caixaPerguntas = document.querySelector(".caixa-perguntas") ;
+const caixaAlternativas = document.querySelector(".caixa-alternativas") ;
+const caixaResultado = document.querySelector(".caixa-resultado") ;
+const textoResultado = document.querySelector(".texto-resultado") ;
+
+const perguntas =[
+    {
+        enunciado : "Pergunta1",
+        alternativas : [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    },
+    {
+        enunciado : "Pergunta2",
+        alternativas : [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    },
+    {
+        enunciado : "Pergunta3",
+        alternativas : [
+            "Alternativa1",
+            "Alternativa2"
+        ]
+    }
+] ;
